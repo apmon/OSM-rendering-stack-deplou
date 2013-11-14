7469f4e13b411fcc17752d40b122ac35d4d0d0a5
Kai Krueger <kakrueger@gmail.com>
2013-11-14 14:23:16 -0700

    Add packaging scripts for saucy stable

---
+++ launchpad/saucy/mapnik-style/update-package.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cp -r /usr/local/src/svn/openstreetmap-carto-verbatim src/
+rm -rf src/.git
+
+cp -r debian/ src/
+
+cd src
+/usr/local/src/svn/carto/bin/carto project.mml > osm.xml
+debuild -S
+cd ..
+
+dput ppa:kakrueger/openstreetmap openstreetmap-mapnik-carto-stylesheet-data_*_source.changes
+ 
+
+rm openstreetmap-mapnik-carto-stylesheet-data_*_source.changes
+rm openstreetmap-mapnik-carto-stylesheet-data_*.dsc
+rm -rf src
+
